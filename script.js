f17eb7a5f187371f71e79c84c63e7647fb36e475
sarad <risingstar3993@gmail.com>
2020-07-21 12:34:04 +0545

    " Validation in form Completed "

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // js for validation of form
 
-// first let's bring everything from the DOM
+
+// first let's import everything from the DOM
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const confirm_password = document.getElementById('confirm_password');
+
 
 // function for showing input error message
 function showError(input, message) {
@@ -22,6 +24,7 @@ function showError(input, message) {
   small.innerText = message; // our required message is passed as a parameter in above function and we display it now in place of default message
 }
 
+
 // function for success (outline must be green)
 // input as a parameter takes the username passed to it
 function showSuccess(input) {
@@ -31,23 +34,26 @@ function showSuccess(input) {
   // and thus if that field is correct and properly validated green bordered outline is shown
 }
 
-// function for validation of email:
-// go to google and search the regular expression for email validation: js email regex
-// and paste it inside this function
-// NOTE: validation must be done in server side as well cuz this validation in js can easily be disabled
 
-function isValidEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
+// function for getting field name during validation
+function getFieldName(input) {
+    // now i want to return the input Id but also uppercase the first letter of that field name
+  
+    // here is little bit tricky part
+    // we first get the initial letter of that field name and then convert it into uppercase ; later we concatenate  the rest of the words of that field name but this time we slice the string and start from 2nd character which means index 1
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  }
+  
 
-// validation/ check required fields
+// validation/check required fields
 function checkRequired(inputArray) {
   // now we have to loop through the array and then check for each inputs
   // forEach is used to loop through the array and we can then perform the task within the loop
   inputArray.forEach(function (input) {
     if (input.value.trim() == '') {
-      showError(input, `${getFieldName(input)} is required`);
+      showError(input, `${getFieldName(input)} is required!`);
+      // for message i have used backticks(``) to pass it as a template string 
+      // trim() to remove any whitespaces 
     } else {
       showSuccess(input);
     }
@@ -55,35 +61,55 @@ function checkRequired(inputArray) {
 }
 
 
-// check input length 
-function checkLength(input,min,max=100){
-
+// check input length for username and password 
+function checkLength(input,min,max){
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters! `);
+    }
+    else if (input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max} characters! `);
+    }
+    else{
+        showSuccess(input);
+    }
 }
 
 
-
-// function for getting field name during validation
-function getFieldName(input) {
-  // now i want to return the input Id but also uppercase the first letter of that field name
-
-  // here is little bit tricky part
-  // we first get the initial letter of that field name and then convert it into uppercase ; later we concatenate  the rest of the words of that field name but this time we slice the string and start from 2nd character which means index 1
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+// function that checks whether both passwords match or not 
+function checkPasswordsMatch(input1,input2){
+    if(input1.value !== input2.value){
+        showError(input2,'Passwords do not match!');
+    }
 }
 
 
+// function for validation of email:
+// go to google and search the regular expression for email validation: js email regex
+// and paste it inside this function
+// NOTE: validation must be done in server side as well cuz this validation in js can easily be disabled
 
+function checkEmail(input) {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(regex.test(input.value.trim())){
+          showSuccess(input);
+      }
+      else {
+          showError(input,`${getFieldName(input)} is not valid!`);
+      }
+  }
+  
 
-// now we need to add the event listener  on the form when we submit it
-// we are listening for submit on this form
+// Now we need to add the event listener  on the form when we submit it
+// we are listening for Submit on this form
 form.addEventListener('submit', function (event) {
   event.preventDefault(); // actually submits the form without flashing it
 
-  // for validation (invoking the function )
   // instead of calling the function four different times ; we call the function once and create a array of inputs
-  checkRequired([username, email, password, password2]);
+  checkRequired([username, email, password, confirm_password]);
   // once we click submit button it's gonna run the checkRequired() function ; pass all the inputs as an array and loop through the array one by one and check the condition mentioned above inside the function
 
-  checkLength(username, 3 , 20); // minimum 3 and max 20 
-  checkLength(password,6);
+  checkLength(username, 4 ,20); // minimum 3 and max 20 
+  checkLength(password,6,30);
+  checkEmail(email);
+  checkPasswordsMatch(password,confirm_password);  
 });
